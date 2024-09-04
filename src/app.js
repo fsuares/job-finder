b5dff485bbd94397a039abd165ae20d1c2ca6d35
Fernando Suares <fernandoaqs@outlook.com.br>
2024-09-04 20:47:34 -0300

    chore: create database connection

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const database = require('./database/connection');
+
+database.authenticate()
+    .then(() => {
+        console.log('sucessfully connected on database!');
+    })
+    .catch(err => {
+        console.log(`db connection error: ${err}.`);
+    });
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`app listening on port ${port}`));
+app.listen(port, () => console.log(`app listening on port ${port}!`));
